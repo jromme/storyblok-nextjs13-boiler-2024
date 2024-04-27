8c9dfa8f58cbc9606ef3100bbab2946a24ef68db
Jérôme Braeken <jeromebraeken@gmail.com>
2024-04-27 18:32:44 +0200

    updates

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -7,30 +7,42 @@ import {
   StoryblokComponent,
 } from "@storyblok/react";
 
-export default function Page({ story, locales, locale, defaultLocale, preview }) {
+export default function Page({
+  story,
+  locales,
+  locale,
+  defaultLocale,
+  preview,
+}) {
   story = useStoryblokState(story, {
-    language: locale
+    language: locale,
   });
 
   return (
-    <div >
+    <div>
       <Head>
         <title>{story ? story.name : "My Site"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout locales={locales} locale={locale} defaultLocale={defaultLocale}>
-        <StoryblokComponent blok={story.content} locale={locale}  />
+        <StoryblokComponent blok={story.content} locale={locale} />
       </Layout>
     </div>
   );
 }
 
-export async function getStaticProps({ params, locales, locale, defaultLocale, preview }) {
+export async function getStaticProps({
+  params,
+  locales,
+  locale,
+  defaultLocale,
+  preview,
+}) {
   let slug = params.slug ? params.slug.join("/") : "home";
 
   let sbParams = {
-    version: preview? "draft" : "published",
-    language: locale
+    version: preview ? "draft" : "published",
+    language: locale,
   };
 
   const storyblokApi = getStoryblokApi();
@@ -38,21 +50,21 @@ export async function getStaticProps({ params, locales, locale, defaultLocale, p
 
   return {
     props: {
-      locales, 
-      locale, 
+      locales,
+      locale,
       defaultLocale,
       story: data ? data.story : false,
       key: data ? data.story.id : false,
-      preview: preview || false
+      preview: preview || false,
     },
     revalidate: 3600,
   };
 }
 
-export async function getStaticPaths({locales}) {
+export async function getStaticPaths({ locales }) {
   const storyblokApi = getStoryblokApi();
-  let { data } = await storyblokApi.get("cdn/links/" ,{
-    version: 'published'
+  let { data } = await storyblokApi.get("cdn/links/", {
+    version: "published",
   });
 
   let paths = [];
@@ -63,7 +75,6 @@ export async function getStaticPaths({locales}) {
 
     const slug = data.links[linkKey].slug;
     let splittedSlug = slug.split("/");
-
 
     for (const locale of locales) {
       paths.push({ params: { slug: splittedSlug }, locale });
